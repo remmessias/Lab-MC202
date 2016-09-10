d1443c75b4b8780c03a76ed5abc81bd67c525269
Rebecca Moreira Messias <r186416@dac.unicamp.br>
2016-09-10 17:19:55 -0300

    The output is correct

--- lab02/main.c
+++ lab02/main.c
@@ -18,7 +18,7 @@
 typedef struct No {
 	int chave;
 	struct No *proximo;
-	struct No *anterior;
+		struct No *anterior;
 } No;
 
 /*
@@ -52,45 +52,196 @@ void insere(No **lista, int numero) {
 }
 
 /*
+ * Função que escreve uma lista na tela.
+ */
+void escreve(No **lista) {
+	No *atual = *lista;
+
+	while (atual->proximo != NULL) {
+		printf("%d ", atual->chave);
+		atual = atual->proximo;
+	}
+	printf("%d\n", atual->chave);
+}
+
+/*
+ * Função que inverte dois nós de uma lista
+ */
+void inverte(No **lista, No *no) {
+	No *proximo = no->proximo;
+	No *anterior = no->anterior;
+
+	if (proximo == NULL) {
+		anterior->anterior->proximo = no;
+		no->anterior = anterior->anterior;
+		no->proximo = anterior;
+		anterior->anterior = no;
+		anterior->proximo = proximo;
+	}
+	else if (anterior->anterior == NULL) {
+		no->anterior = NULL;
+		no->proximo = anterior;
+		anterior->anterior = no;
+		anterior->proximo = proximo;
+		proximo->anterior = anterior;
+		(*lista) = no;
+	}
+	else {
+		anterior->anterior->proximo = no;
+		no->anterior = anterior->anterior;
+		no->proximo = anterior;
+		anterior->anterior = no;
+		anterior->proximo = proximo;
+		proximo->anterior = anterior;
+	}
+}
+
+/*
+ * Função que coloca um nó no começo da lista
+ */
+void colocaComeco(No **lista, No *no) {
+
+	if (no->proximo == NULL) {
+		no->anterior->proximo = no->proximo;
+		no->anterior = NULL;
+		no->proximo = *lista;
+		(*lista)->anterior = no;
+	}
+	else {
+		no->anterior->proximo = no->proximo;
+		no->proximo->anterior = no->anterior;
+		no->anterior = NULL;
+		no->proximo = *lista;
+		(*lista)->anterior = no;
+	}
+}
+
+/*
  * Função que acessa uma chave em uma lista do jeito MTF
  */
 int acessaMTF(No **lista, int chave) {
-	return 0;
+	int custo = 0;
+	No *atual;
+	for (atual = *lista; atual->chave != chave; atual = atual->proximo) {
+		custo++;
+	}
+	custo++;
+
+	if (custo > 1)
+		colocaComeco(lista, atual);
+
+	*lista = atual;
+
+	return custo;
 }
 
 /*
  * Função que acessa uma chave em uma lista do jeito TR
  */
 int acessaTR(No **lista, int chave) {
-	return 0;
+	int custo = 0;
+	No *atual;
+	for (atual = *lista; atual->chave != chave; atual = atual->proximo) {
+		custo++;
+	}
+	custo++;
+
+	if (custo > 1)
+		inverte(lista, atual);
+	return custo;
 }
 
 /*
  * Função que insere uma chave em uma lista do jeito MTF
  */
 int insereMTF(No **lista, int chave) {
-	return 0;
+	No *novo, *atual = *lista;
+	int custo = 0;
+	novo = malloc(sizeof(No));
+
+	while (atual->proximo != NULL) {
+		custo++;
+		atual = atual->proximo;
+	}
+	custo += 2;
+	novo->chave = chave;
+	novo->proximo = NULL;
+	novo->anterior = atual;
+	atual->proximo = novo;
+
+	return custo;
 }
 
 /*
  * Função que insere uma chave em uma lista do jeito TR
  */
 int insereTR(No **lista, int chave) {
-	return 0;
+	No *novo, *atual = *lista;
+	int custo = 0;
+	novo = malloc(sizeof(No));
+
+	while (atual->proximo != NULL) {
+		custo++;
+		atual = atual->proximo;
+	}
+	custo += 2;
+	novo->chave = chave;
+	novo->proximo = NULL;
+	novo->anterior = atual;
+	atual->proximo = novo;
+
+	inverte(lista, novo);
+
+	return custo;
 }
 
 /*
  * Função que remove uma chave em uma lista do jeito MTF
  */
 int removeMTF(No **lista, int chave) {
-	return 0;
+	No *atual = *lista;
+	int custo = 0;
+
+	while (atual->chave != chave) {
+		custo++;
+		atual = atual->proximo;
+	}
+	custo++;
+	atual->proximo->anterior = atual->anterior;
+	atual->anterior->proximo = atual->proximo;
+	free(atual);
+
+	return custo;
 }
 
 /*
  * Função que remove uma chave em uma lista do jeito TR
  */
 int removeTR(No **lista, int chave) {
-	return 0;
+	No *atual = *lista;
+	int custo = 0;
+	while (atual->chave != chave) {
+		custo++;
+		atual = atual->proximo;
+	}
+	custo++;
+	if (atual->anterior == NULL) {
+		atual->proximo->anterior = NULL;
+		(*lista) = atual->proximo;
+		free(atual);
+	}
+	else if (atual->proximo == NULL) {
+		atual->anterior->proximo = NULL;
+		free(atual);
+	}
+	else {
+		atual->proximo->anterior = atual->anterior;
+		atual->anterior->proximo = atual->proximo;
+		free(atual);
+	}
+
+
+	return custo;
 }
 
 /*
@@ -106,15 +257,12 @@ void leLista(No **listaMTF, No **listaTR, int tamanho) {
 }
 
 /*
- * Função que escreve uma lista ou uma sublista na tela.
+ * Função que verifica se uma lista esta vazia
  */
-void escreve(No **lista) {
-	No *atual = *lista;
-
-	while (atual != NULL) {
-		printf("%d ", atual->chave);
-		atual = atual->proximo;
-	}
+int estaVazia(No **lista) {
+	if ((*lista) != NULL)
+		return 0;
+	return 1;
 }
 
 /*
@@ -132,6 +280,22 @@ void destruir(No **lista) {
 
 ////////////////////// FIM OPERAÇÕES TRIVIAIS COM A LISTA
 
+/*
+ * Função que escreve a saída na tela para o usuário
+ */
+void escreveSaida(No **listaMTF, No **listaTR, int custoMTF, int custoTR) {
+	printf("%d\n", custoMTF);
+	if (estaVazia(listaMTF))
+		printf("\n");
+	else
+		escreve(listaMTF);
+	printf("%d\n", custoTR);
+	if (estaVazia(listaTR))
+		printf("\n");
+	else
+		escreve(listaTR);
+}
+
 void leInstrucoes(No **listaMTF, No **listaTR, int requisicoes) {
 	int custoMTF = 0, custoTR = 0, chave, i;
 	char acao;
@@ -140,27 +304,23 @@ void leInstrucoes(No **listaMTF, No **listaTR, int requisicoes) {
 		scanf(" %c %d", &acao, &chave);
 		switch (acao) {
 			case ACESSAR:
-				//MTF
 				custoMTF += acessaMTF(listaMTF, chave);
-				//TR
 				custoTR += acessaTR(listaTR, chave);
 				break;
 			case INSERIR:
-				//MTF
 				custoMTF += insereMTF(listaMTF, chave);
-				//TR
 				custoTR += insereTR(listaTR, chave);
 				break;
 			case REMOVER:
-				//MTF
 				custoMTF += removeMTF(listaMTF, chave);
-				//TR
 				custoTR += removeTR(listaTR, chave);
 				break;
 			default:
 				break;
 		}
 	}
+
+	escreveSaida(listaMTF, listaTR, custoMTF, custoTR);
 }
 
 int main() {
