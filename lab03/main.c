b6f211a8e31a59bfe25b7ffa776831949304aef7
Rebecca Moreira Messias <r186416@dac.unicamp.br>
2016-09-17 16:49:58 -0300

    Need to print the result and fix some bugs

--- lab03/main.c
+++ lab03/main.c
@@ -5,55 +5,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef  struct NoPilha {
-	int dado;
+#define VERMELHO 'v'
+#define AZUL 'a'
+#define SAIDA "sequencia invalida ou nao pode colorir\n"
+
+//////////////////////////////////////////////////////////////
+//                                                          //
+//	   Definição e implementação de operações numa pilha    //
+//                                                          //
+//////////////////////////////////////////////////////////////
+
+// Struct que contém as informações do nó
+typedef struct {
+	int brinquedo, antecessor;
+	char cor;
+} Informacoes;
+
+// Struct que representa cada nó da pilha
+typedef struct NoPilha {
+	Informacoes info;
 	struct NoPilha *prox;
 } NoPilha;
 
+/*
+ * Função que cria uma pilha
+ */
 void criar(NoPilha **p) {
 	*p = NULL;
 }
 
-void empilha (NoPilha **p, int dado) {
+/*
+ * Função que empilha um nó
+ */
+void empilha(NoPilha **p, Informacoes info) {
 	NoPilha *q = malloc(sizeof(NoPilha));
-	q->dado = dado;
+	q->info = info;
 	q->prox = *p;
 	*p = q;
 }
 
-int desempilha (NoPilha **p) {
+/*
+ * Função que desempilha um nó
+ */
+Informacoes desempilha(NoPilha **p) {
 	NoPilha *q = *p;
 
 	if (q != NULL) {
-		int dado = q->dado;
+		Informacoes info = q->info;
 		*p = q->prox;
 		free(q);
-		return dado;
+		return info;
 	}
+	Informacoes retorno;
+	retorno.cor = 'c';
+	retorno.brinquedo = 0;
+	retorno.antecessor = 0;
 
+	return retorno;
+}
+
+/*
+ * Função que retorna o nó do topo da pilha
+ */
+int getTopo(NoPilha **p) {
+	if (*p == NULL)
+		return 0;
+	return (*p)->info.brinquedo;
+}
+
+/*
+ * Função que verifica se uma pilha esta vazia
+ */
+int estaVazia(NoPilha **pilha) {
+	if ((*pilha) == NULL)
+		return 1;
 	return 0;
 }
 
-int procura (NoPilha **p, int dado) {
-	for (NoPilha *q = *p; q != NULL; q = q->prox) {
-		if (q->dado == dado) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-void escreve (NoPilha **p) {
-	for (NoPilha *q = *p; q != NULL; q = q->prox) {
-		if (q->prox == NULL)
-			printf("%d\n", q->dado);
-		else
-			printf("%d ", q->dado);
-	}
-}
-
-void destroi (NoPilha **p) {
+/*
+ * Função que destroi a pilha
+ */
+void destroi(NoPilha **p) {
 	NoPilha *prox;
 
 	for (NoPilha *q = *p; q != NULL; q = prox) {
@@ -62,53 +94,121 @@ void destroi (NoPilha **p) {
 	}
 }
 
-int absoluto (int numero) {
+// FIM DA DEFINIÇÃO E OPERAÇÕES COM A PILHA
+
+/*
+ * Função que verifica quantos brinquedos tem dentro de uma boneca
+ */
+int quantosAzuis(NoPilha **p, int dado) {
+	int qtdAzuis = 0;
+	NoPilha *pilha = *p;
+
+	if (pilha == NULL)
+		return 0;
+
+	for (; pilha != NULL; pilha = pilha->prox) {
+		if (pilha->info.antecessor == -dado && pilha->info.cor == AZUL) {
+			qtdAzuis++;
+		}
+	}
+
+	return qtdAzuis;
+}
+
+/*
+ * Função que verifica se brinquedos com o mesmo número possuem mesma cor
+ */
+int verificarCor(NoPilha **lista, char cor, int elemento) {
+	for (NoPilha *aux = *lista; aux != NULL; aux = aux->prox) {
+		if (aux->info.brinquedo == elemento && aux->info.cor != cor)
+			return 0;
+	}
+	return 1;
+}
+
+/*
+ * Função que tira módulo de um número
+ */
+int absoluto(int numero) {
 	if (numero < 0)
 		return -numero;
-
 	return numero;
 }
 
-int main () {
-	NoPilha *pilhaBrinquedo;
-	int tamanho, elemento, desempilhado, i;
+/*
+ * Função que prepara uma variavel informação para ser empilhada
+ */
+void empilhaInformacao(int elemento, NoPilha **pilhaBrinquedo) {
+	Informacoes informacao;
+	informacao.brinquedo = elemento;
+	informacao.antecessor = getTopo(pilhaBrinquedo);
+	empilha(pilhaBrinquedo, informacao);
+}
 
-	criar(&pilhaBrinquedo);
+/*
+ * Função que seta cor
+ */
+void leEColacaCor(NoPilha **pilhaBrinquedo, NoPilha **cores, int tamanho) {
+	int elemento, qtdAzul, i;
+	Informacoes desempilhado;
 
-	scanf("%d", &tamanho);
-
-	 /*
-	  * 1- Ler N números (N é a variável tamanho)
-	  * 	1.1- Se o número for negativo empilhar
-	  * 	1.2- Se for positivo desempilhar
-	  * 	1.3- verificar se o módulo são iguais
-	  * 		1.2.1- Se for
-	  *
-	  */
-
-
-	for (i = 0; i < tamanho; i ++) {
+	for (i = 0; i < tamanho; i++) {
 		scanf(" %d", &elemento);
-		if (elemento < 0)
-			empilha(&pilhaBrinquedo, elemento);
+		if (elemento < 0) {
+			empilhaInformacao(elemento, pilhaBrinquedo);
+		}
 		else {
-			desempilhado = desempilha(&pilhaBrinquedo);
+			desempilhado = desempilha(pilhaBrinquedo);
+			if (absoluto(desempilhado.brinquedo) == elemento) {
+				qtdAzul = quantosAzuis(cores, elemento);
 
-			if (absoluto(desempilhado) == elemento) {
-
+				if ((qtdAzul + elemento) % 2 == 0) {
+					if (verificarCor(cores, AZUL, elemento)) {
+						desempilhado.cor = AZUL;
+						empilha(cores, desempilhado);
+					}
+					else {
+						printf(SAIDA);
+						break;
+					}
+				}
+				else {
+					if (verificarCor(cores, VERMELHO, elemento)) {
+						desempilhado.cor = VERMELHO;
+						empilha(cores, desempilhado);
+					}
+					else {
+						printf(SAIDA);
+						break;
+					}
+				}
 			}
 			else {
-				printf("sequencia invalida ou nao pode colorir");
+				printf(SAIDA);
 				break;
 			}
 		}
 	}
 
+	if (!estaVazia(pilhaBrinquedo)) {
+		printf(SAIDA);
+	}
+}
 
+int main() {
+	NoPilha *pilhaBrinquedo;
+	NoPilha *cores;
+	int tamanho;
 
+	criar(&pilhaBrinquedo);
+	criar(&cores);
 
+	scanf("%d", &tamanho);
 
+	leEColacaCor(&pilhaBrinquedo, &cores, tamanho);
+
+	destroi(&pilhaBrinquedo);
+	destroi(&cores);
 
 	return 0;
 }
-
